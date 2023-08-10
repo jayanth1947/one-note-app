9c5e9b2e1b4550f0dc0a3f3dd418b2bafdaec58d
jayanth1947 <jayanthonteru111@gmail.com>
2023-08-10 15:13:51 +0530

    added a new modal component file and also added few methods inside oneNote

--- force-app/main/default/lwc/oneNote/oneNote.js
+++ force-app/main/default/lwc/oneNote/oneNote.js
@@ -1,3 +1,12 @@
 import { LightningElement } from 'lwc';
 
-export default class OneNote extends LightningElement {}
+export default class OneNote extends LightningElement {
+    showModal=false;
+    createNoteHandler(){
+        this.showModal=true;
+    }
+
+    closeModalHandler(){
+        this.showModal=false;
+    }
+}
