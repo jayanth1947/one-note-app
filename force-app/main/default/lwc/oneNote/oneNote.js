214bacea1fb44b5969d6e9a268c76bf83120e496
jayanth1947 <jayanthonteru111@gmail.com>
2023-08-10 20:10:09 +0530

    modified modal component

--- force-app/main/default/lwc/oneNote/oneNote.js
+++ force-app/main/default/lwc/oneNote/oneNote.js
@@ -1,12 +1,52 @@
 import { LightningElement } from 'lwc';
 
+
+const DEFAULT_NOTE_FORM={
+    Name:"",
+    Description__c	:""
+}
 export default class OneNote extends LightningElement {
-    showModal=false;
+
+    showModal=false
+    noteRecord=DEFAULT_NOTE_FORM
+    formats = [
+        'font',
+        'size',
+        'bold',
+        'italic',
+        'underline',
+        'strike',
+        'list',
+        'indent',
+        'align',
+        'link',
+        'clean',
+        'table',
+        'header',
+        'color',
+    ];
+
+    get isFormInvalid(){
+        return !(this.noteRecord && this.noteRecord.Name && this.noteRecord.Description__c)
+    }
     createNoteHandler(){
-        this.showModal=true;
+        this.showModal=true
     }
 
     closeModalHandler(){
-        this.showModal=false;
+        this.showModal=false
+        this.noteRecord=DEFAULT_NOTE_FORM
+    }
+
+    changeHandler(event){
+        const {name,value}=event.target
+        // const name=event.target.name;
+        // const value=event.target.value;
+        this.noteRecord={...this.noteRecord,[name]:value}
+    }
+
+    submitHandler(event){
+        event.preventDefault();
+        console.log("this.noteRecord", JSON.stringify( this.noteRecord))
     }
 }
