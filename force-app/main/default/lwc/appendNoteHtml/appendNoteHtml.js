61b0b47b4fbed460fd8d51474f0dfb0c2617baa2
jayanth1947 <jayanthonteru111@gmail.com>
2023-08-16 12:13:42 +0530

    added appendNoteHtml file
    This file will format the description of the note

---
+++ force-app/main/default/lwc/appendNoteHtml/appendNoteHtml.js
@@ -0,0 +1,31 @@
+import { api, LightningElement } from 'lwc';
+
+export default class AppendNoteHtml extends LightningElement {
+
+    _result
+    loaded
+
+
+    @api
+    get result(){
+        return this._result
+    }
+
+    set result(data){
+        this._result=data
+    }
+
+    renderedCallback(){
+        if(this._result && !this.loaded){
+            this.attachHtml()
+        }
+    }
+    attachHtml(){
+        const container =this.template.querySelector('.htmlcontainer')
+        if(container){
+            container.innerHTML=this.result
+            this.loaded=true
+        }
+    }
+
+}
